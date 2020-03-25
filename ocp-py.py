e2fb241e7cc7429c11c1d8a2f8381b36b94a67bc
sysThematic <57949654+sysThematic@users.noreply.github.com>
2020-03-25 11:30:40 +0300

    Update ocp-py.py

--- ocp-py.py
+++ ocp-py.py
@@ -8,11 +8,14 @@ url = 'http://10.81.97.31:8000/index.txt'
 while(True):
   try:
     r = requests.get(url)
-  except requests.exceptions.Timeout:
-    print ("Timeout")
+  except requests.exceptions.Timeout as errt:
+    print ("Timeout! \n", errt)
+  except requests.exceptions.ConnectionError as errc:
+    print ("Connection Refused! is web server up?\n", errc)
   except requests.exceptions.RequestException as e:
     # catastrophic error. bail.
-    raise SystemExit(e)
+    print ("OOps: Something Else...\n",e)
+  else:
+    print (r.text)
 
-  print (r.text)
   time.sleep(60)
