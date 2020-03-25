8a9f4acfa2e88e54757ee97117f5dfecb232fed8
sysThematic <57949654+sysThematic@users.noreply.github.com>
2020-03-25 11:16:44 +0300

    Update ocp-py.py

--- ocp-py.py
+++ ocp-py.py
@@ -1,6 +1,18 @@
 #!/bin/python3
 
+import time
 import requests
+
 url = 'http://10.81.97.31:8000/index.txt'
-r = requests.get(url)
-print (r.text)
+
+while(True):
+  try:
+    r = requests.get(url)
+  except requests.exceptions.Timeout:
+    print ("Timeout")
+  except requests.exceptions.RequestException as e:
+    # catastrophic error. bail.
+    raise SystemExit(e)
+
+  print (r.text)
+  time.sleep(60)
